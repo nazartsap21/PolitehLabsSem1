3b0a4ab09213fdecf70fdc03095158854348fb15
nazartsap21 <nazar.tsap.ua@gmail.com>
2023-11-11 18:17:28 +0200

    First lab commit

---
+++ lab1.py
@@ -0,0 +1,21 @@
+import math
+
+sport = "football"
+print(sport)
+
+is_student = True
+is_teacher = False
+
+print(is_student and is_teacher)
+print(is_student or is_teacher)
+print(is_student is not is_teacher)
+
+is_hot = False
+is_cold = True
+
+print(is_cold or is_hot)
+
+x = 0.357
+y = 2.037
+
+print(math.pow(x, math.cos(y)) - (math.pow(math.tan(x), math.sqrt(y - x))) - 19.12 * math.log(math.pow(y,3)))
